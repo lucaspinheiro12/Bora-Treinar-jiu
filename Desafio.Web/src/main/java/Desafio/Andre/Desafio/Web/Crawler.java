4b0dbadd9159485a721e436d2d9cbdfe13fa770c
Lucas <cordeiropinheirolucas@gmail.com>
2023-03-26 22:02:26 -0300

    conferindo o codigo

--- Desafio.Web/src/main/java/Desafio/Andre/Desafio/Web/Crawler.java
+++ Desafio.Web/src/main/java/Desafio/Andre/Desafio/Web/Crawler.java
@@ -13,33 +13,24 @@ public class Crawler {
 
 	public static void main(String[] args) throws IOException {
 		String url = "https://g1.globo.com/";
+		String url2 = "https://g1.globo.com/economia/";
+		String url3 = "https://g1.globo.com/economia/agronegocios/";
+		crawl(url2, new ArrayList<String>());
 		crawl(url, new ArrayList<String>());
-	}
-
-	private static void crawl( String url, ArrayList<String> visita) throws IOException{
-
-		Document doc = request(url, visita);
-		System.out.println(visita.size());
-		if(doc != null) {
-			for(Element link : doc.select("a[href")) {
-				String proximoLink = link.absUrl("href");
-				if(visita.contains(proximoLink) == false) {
-					if(visita.size() <= 3) {
-						crawl( proximoLink, visita);
-					}
-				}
-			}
-		}
+		crawl(url3, new ArrayList<String>());
 		System.out.println("Encerrado.");
 	}
-	
-	private static Document request(String url, ArrayList<String> v) {
+
+	private static Document crawl( String url, ArrayList<String> visita) throws IOException{
+
 		try {
 			Connection con = Jsoup.connect(url);
 			Document doc = con.get();
+			
 			Elements div = doc.getElementById("bstn-fd-launcher").select(".feed-root").select(".feed-placeholder").select("._evt").select(".bstn-fd").select("._evg")
 					.select("._evt").select(".bastian-page").select("._evg").select("._evt")
 					.select(".bastian-feed-item").select(".bstn-item-shape").select(".feed-post-body");
+			
 			if(con.response().statusCode() == 200) {			
 				for(Element elemento : div) {
 					String urlDentro = elemento.select(".feed-post-body-title").select("._evt > h2 > a").first().attr("href");
@@ -61,7 +52,7 @@ public class Crawler {
 					System.out.println("Data de publicação: " + data);
 					System.out.println();
 				}
-				v.add(url);
+				visita.add(url);
 				return doc;	
 			}
 			return null;
@@ -69,8 +60,5 @@ public class Crawler {
 			System.out.println("O ERRO É : " + e.getMessage());;
 			return null;
 		}
-
-	}
-	
-	
+	}	
 }
